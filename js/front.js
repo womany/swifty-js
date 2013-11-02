f0f91c7f0eec5a207aa0300629424d5b73db62ec
Japie <jaywiewu@email.com>
2013-11-03 05:21:00 +0800

    Can extend, style is not ok

--- js/front.js
+++ js/front.js
@@ -25,6 +25,7 @@ document.onmouseup = function() {
   // [!!] click submit button should not do anything.
   // if (content.rangeCount > 0 && boxVanish == true) {
   if (content.rangeCount > 0 && boxVanish) {
+    shareEventHandler();
     var range = content.getRangeAt(0);
     if (range.toString()) {
       console.log(event.clientX);
@@ -44,32 +45,61 @@ document.onmouseup = function() {
 };
 
 function createDivs(content) {
-  var text = document.createTextNode('Swifttttttttttty');
+  // annotation
   var input = document.createElement('input');
   input.type = 'text';
   input.id = 'annotation';
   // input.value = 'add some annotation...';
+
+  // save
   var submit = document.createElement('div');
   submit.id = 'submit';
-  var submitText = document.createTextNode('收藏！');
+  var submitText = document.createTextNode('Save');
   submit.appendChild(submitText);
 
+  // fb_share
   var share = document.createElement('input');
   share.type = 'checkbox';
   share.id = 'share';
   share.value = 1;
   // <input type="checkbox" name="vehicle" value="Car">I have a car 
 
-  var more = document.createElement('div');
-  more.id = 'more';
-  more.value = 1;
+  // var more = document.createElement('div');
+  // more.id = 'more';
+  // more.value = 1;
+
+  // extend_imgs
+  var imgfull = document.createElement('img');
+  imgfull.src = chrome.extension.getURL('images/resize-full.png');
+  imgfull.id = 'resize-full';
+  var img_container = document.createElement('div');
+  img_container.id = 'img_container_f';
+  img_container.appendChild(imgfull);
+
+  var imgsmall = document.createElement('img');
+  imgsmall.src = chrome.extension.getURL('images/resize-small.png');
+  imgsmall.id = 'resize-small';
+  var img_container_2 = document.createElement('div');
+  img_container_2.id = 'img_container_s';
+  img_container_2.appendChild(imgsmall);
+  
+  // tag
+  var tag = document.createElement('input');
+  tag.type = 'text';
+  tag.id = 'tag';
+
+  var div_more = document.createElement('div');
+  div_more.appendChild(input);
+  div_more.appendChild(tag);
+  div_more.appendChild(share);
+  div_more.id = 'div_more';
 
   var div = document.createElement('div');
-  div.appendChild(text);
-  div.appendChild(input);
-  div.appendChild(share);
+  div.appendChild(div_more);
   div.appendChild(submit);
-  div.appendChild(more);
+  // div.appendChild(more);
+  div.appendChild(img_container);
+  div.appendChild(img_container_2);
   div.id = 'swifty-light-box';
   
   if (document.body.firstChild){
@@ -77,7 +107,7 @@ function createDivs(content) {
   } else {
     document.body.appendChild(div);
   }
-  
+  initialBox()
   submitDiv(div,content);
 }
 function submitDiv(div,content){
@@ -105,6 +135,7 @@ getEmail = function(content){
         var title = $(document).attr("title");
         var annotation = $('#swifty-light-box input').val();
         var share = $('#swifty-light-box #share').val();
+        var tag = $('#swifty-light-box #tag').val();
         console.log($('#swifty-light-box input'));
         console.log($('#swifty-light-box input').val());
         console.log('content:',content);
@@ -121,7 +152,8 @@ getEmail = function(content){
             content: content,
             title: title,
             url: url,
-            annotation: annotation
+            annotation: annotation,
+            tag: tag
           },
           dataType: 'json',
           success: function(data) {
@@ -135,4 +167,39 @@ getEmail = function(content){
         boxVanish = true;
     });
   });
+}
+
+function initialBox(){
+  console.log('initialBoooooooox');
+  // initial
+  $('#img_container_s').addClass('display-none');
+  $('#div_more').addClass('display-none');
+  $('#swifty-light-box').addClass('box-small');
+  $('#submit').addClass('submit-small');
+}
+
+
+function shareEventHandler() {
+  console.log('yooshareEventHandler');
+  // $('#img_container_s').addClass('display-none');
+  // $('#div_more').addClass('display-none');
+
+  $('#img_container_f').on('click',function(){
+    $('#swifty-light-box').remove();
+    initialBox();
+    boxVanish = false;
+    console.log('extenddddd');
+
+    $(this).addClass('display-none');
+    $('#img_container_s').removeClass('display-none');
+    $('#div_more').removeClass('display-none');
+
+    $('#swifty-light-box').removeClass('box-small');
+    $('#swifty-light-box').addClass('box-full');
+
+    $('#submit').removeClass('submit-small');
+    $('#submit').addClass('submit-full');
+
+    // $('#annotation').addClass('')
+  })  
 }
--- js/share.js
+++ js/share.js
@@ -1,1 +1,6 @@
 console.log('I am share.js!');
+
+(function(){
+  console.log($('#img_container_s'));
+  $('#img_container_s').addClass('display-none');
+})();
